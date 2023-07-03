9ea3aa256d7ec24a8b30baf3e6c06daa529a6669
RoKo159 <107150695+RoKo159@users.noreply.github.com>
2023-07-03 20:41:27 +0200

    fix2

--- src/test/java/pl/kurs/geometricshapes/controllers/ShapeControllerTest.java
+++ src/test/java/pl/kurs/geometricshapes/controllers/ShapeControllerTest.java
@@ -31,7 +31,7 @@ import static org.mockito.Mockito.when;
 
 
 @AutoConfigureMockMvc
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@SpringBootTest
 public class ShapeControllerTest {
 
     ModelMapper modelMapper = new ModelMapper();
